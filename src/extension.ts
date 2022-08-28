f923b4e2afea06d07c7019aad2bf0b0995dccb49
Shyam Arora <shyarora7@gmail.com>
2022-08-29 00:51:37 +0530

    Test

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,16 @@
 import vscode from "vscode";
 import { infiniteProgress, installExtension, showReloadPrompt } from "./utils";
 
-export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand("shyarora-demo.setup-installer", async () => {
-        const progress = infiniteProgress("Please wait while extension is installing");
-        try {
-            await installExtension(context.globalStorageUri.path);
-            showReloadPrompt("Installation complete, please reload");
-        } catch (e) {
-            vscode.window.showErrorMessage(`Error occured while installing extension ${e}`);
-        } finally {
-            progress.finish();
-        }
-    });
-
-    context.subscriptions.push(disposable);
+export async function activate(context: vscode.ExtensionContext) {
+    const progress = infiniteProgress("Please wait while setting up...");
+    try {
+        await installExtension(context.globalStorageUri.path);
+        showReloadPrompt("Installation complete, please reload");
+    } catch (e) {
+        vscode.window.showErrorMessage(`Error occured while installing extension ${e}`);
+    } finally {
+        progress.finish();
+    }
 }
 
 // this method is called when your extension is deactivated

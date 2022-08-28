57e1793a604a6d488704c0a4954999da758e8a98
Shyam Arora <shyarora7@gmail.com>
2022-08-29 00:16:09 +0530

    Done

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@
  *
  */
 
-export const VSXI_FILE_URL = "https://github.com/shyam-arora/shyarora-demo/raw/main/shyarora-demo-file-0.0.2.vsix";
+export const VSXI_FILE_URL = "https://github.com/shyam-arora/shyarora-demo/raw/main/shyarora-demo-file-1.0.0.vsix";
--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,17 @@
 import vscode from "vscode";
-import { infiniteProgress, installExtension } from "./utils";
+import { infiniteProgress, installExtension, showReloadPrompt } from "./utils";
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Congratulations, your extension "shyarora-demo" is now active!');
-
-    // let disposable = vscode.commands.registerCommand("shyarora-demo.helloWorld", async () => {
-    //     const progress = infiniteProgress("Please wait while extension is installing");
-    //     try {
-    //         await installExtension(context.globalStorageUri.path);
-    //     } catch (e) {
-    //         vscode.window.showErrorMessage(`Error occured while installing extension ${e}`);
-    //     } finally {
-    //         progress.finish();
-    //     }
-    // });
-
-    let disposable = vscode.commands.registerCommand("shyarora-demo-file.info-command", async () => {
-        vscode.window.showInformationMessage("This is just info-command");
+    let disposable = vscode.commands.registerCommand("shyarora-demo.setup-installer", async () => {
+        const progress = infiniteProgress("Please wait while extension is installing");
+        try {
+            await installExtension(context.globalStorageUri.path);
+            showReloadPrompt("Installation complete, please reload");
+        } catch (e) {
+            vscode.window.showErrorMessage(`Error occured while installing extension ${e}`);
+        } finally {
+            progress.finish();
+        }
     });
 
     context.subscriptions.push(disposable);

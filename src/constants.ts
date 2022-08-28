f27e731fd8cbff72f6992cb7a9807848c68da51e
Shyam Arora <shyarora7@gmail.com>
2022-08-29 00:12:14 +0530

    V1 added

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@
  *
  */
 
-export const VSXI_FILE_URL = "https://github.com/shyam-arora/shyarora-demo/raw/main/shyarora-demo-test-0.0.1.vsix";
+export const VSXI_FILE_URL = "https://github.com/shyam-arora/shyarora-demo/raw/main/shyarora-demo-file-0.0.2.vsix";
--- src/extension.ts
+++ src/extension.ts
@@ -4,15 +4,19 @@ import { infiniteProgress, installExtension } from "./utils";
 export function activate(context: vscode.ExtensionContext) {
     console.log('Congratulations, your extension "shyarora-demo" is now active!');
 
-    let disposable = vscode.commands.registerCommand("shyarora-demo.helloWorld", async () => {
-        const progress = infiniteProgress("Please wait while extension is installing");
-        try {
-            await installExtension(context.globalStorageUri.path);
-        } catch (e) {
-            vscode.window.showErrorMessage(`Error occured while installing extension ${e}`);
-        } finally {
-            progress.finish();
-        }
+    // let disposable = vscode.commands.registerCommand("shyarora-demo.helloWorld", async () => {
+    //     const progress = infiniteProgress("Please wait while extension is installing");
+    //     try {
+    //         await installExtension(context.globalStorageUri.path);
+    //     } catch (e) {
+    //         vscode.window.showErrorMessage(`Error occured while installing extension ${e}`);
+    //     } finally {
+    //         progress.finish();
+    //     }
+    // });
+
+    let disposable = vscode.commands.registerCommand("shyarora-demo-file.info-command", async () => {
+        vscode.window.showInformationMessage("This is just info-command");
     });
 
     context.subscriptions.push(disposable);
